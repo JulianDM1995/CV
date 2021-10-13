732ba6d91989ff25ab865ab2048ad1e589feed7f
Julian Medina <julian.medina@truepill.com>
2021-10-13 10:29:34 -0500

    wip

--- server-user-config.sh
+++ server-user-config.sh
@@ -7,4 +7,8 @@ ufw status
 rsync --archive --chown=julian:julian ~/.ssh /home/julian
 su julian
 
+git clone https://github.com/JulianDM1995/CV
+
+sudo apt install docker-compose
+
 # ssh julian@147.182.201.172
